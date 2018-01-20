421bc7e7e85c2351b33b9420bc0396b84efd971c
jaf7 <janthonyfields@gmail.com>
2018-01-19 19:19:01 -0500

    built hash reformatter that iterates nested hash

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+def reformat_languages(programming_languages)
+  new_hash = {}
+
+  programming_languages.each do |language_style, languages|
+    # binding.pry
+    languages.each do |language, attributes|
+      # binding.pry
+      attributes.each do |type_key, value|
+        # binding.pry
+
+        if !new_hash[language]
+          new_hash[language] = {}
+        end
+
+        if !new_hash[language][type_key]
+          new_hash[language][type_key] = value
+        end
+
+        new_hash[language][:style] ||= []
+        new_hash[language][:style] << language_style
+      end
+    end
+  end
+
+  new_hash
 end
